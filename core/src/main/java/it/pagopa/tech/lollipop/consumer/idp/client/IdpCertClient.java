65e29c314678b71c8f54bfd65e548cfeeb46c4c4
svariant <samuele.varianti@nttdata.com>
2023-04-11 16:46:38 +0200

    [SLS-19] Defined skeleton getCertData - changed return type to List

--- core/src/main/java/it/pagopa/tech/lollipop/consumer/idp/client/IdpCertClient.java
+++ core/src/main/java/it/pagopa/tech/lollipop/consumer/idp/client/IdpCertClient.java
@@ -2,8 +2,9 @@
 package it.pagopa.tech.lollipop.consumer.idp.client;
 
 import it.pagopa.tech.lollipop.consumer.model.IdpCertData;
+import java.util.List;
 
 public interface IdpCertClient {
 
-    IdpCertData getCertData(String entityId, String instant);
+    List<IdpCertData> getCertData(String entityId, String instant);
 }
