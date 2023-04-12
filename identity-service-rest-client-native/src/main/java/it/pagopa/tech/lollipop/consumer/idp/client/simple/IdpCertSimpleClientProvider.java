27ca9191a709ee4134d6658bbfb033a66c0cfa0c
svariant <samuele.varianti@nttdata.com>
2023-04-12 14:56:49 +0200

    [SLS-19] Completed signature retrieving from idp certification - added comments & cleaned models

--- identity-service-rest-client-native/src/main/java/it/pagopa/tech/lollipop/consumer/idp/client/simple/IdpCertSimpleClientProvider.java
+++ identity-service-rest-client-native/src/main/java/it/pagopa/tech/lollipop/consumer/idp/client/simple/IdpCertSimpleClientProvider.java
@@ -5,6 +5,7 @@ import it.pagopa.tech.lollipop.consumer.idp.client.IdpCertClient;
 import it.pagopa.tech.lollipop.consumer.idp.client.IdpCertClientProvider;
 import it.pagopa.tech.lollipop.consumer.idp.client.simple.internal.ApiClient;
 
+/** Provider class for retrieving an instance of {@link IdpCertSimpleClient} */
 public class IdpCertSimpleClientProvider implements IdpCertClientProvider {
 
     /**
