1bdbd7b546e368342cb94addc91c3ac33d3f5e6b
svariant <samuele.varianti@nttdata.com>
2023-04-12 12:30:16 +0200

    [SLS-19] Test covered all IdpCertSimpleClient class methods

--- identity-service-rest-client-native/src/test/java/simple/IdpCertSimpleClientTest.java
+++ identity-service-rest-client-native/src/test/java/simple/IdpCertSimpleClientTest.java
@@ -3,6 +3,8 @@ package simple;
 
 import it.pagopa.tech.lollipop.consumer.exception.CertDataNotFoundException;
 import it.pagopa.tech.lollipop.consumer.exception.CertDataTagListNotFoundException;
+import it.pagopa.tech.lollipop.consumer.exception.EntityIdNotFoundException;
+import it.pagopa.tech.lollipop.consumer.exception.TagListSearchOutOfBoundException;
 import it.pagopa.tech.lollipop.consumer.idp.client.simple.IdpCertSimpleClient;
 import it.pagopa.tech.lollipop.consumer.idp.client.simple.internal.ApiClient;
 import it.pagopa.tech.lollipop.consumer.model.IdpCertData;
@@ -17,9 +19,12 @@ class IdpCertSimpleClientTest {
 
     private static IdpCertSimpleClient idpCertSimpleClient;
 
-    private static final String instant = "1679072970";
+    private static final String INSTANT = "1679072970";
     private static final String SPID_ENTITY_ID = "https://posteid.poste.it";
     private static final String CIE_ENTITY_ID = "https://idserver.servizicie.interno.gov.it/idp/profile/SAML2/POST/SSO";
+
+    private static final String WRONG_ENTITY_ID = "https://wrongEntityID.it";
+    private static final String WRONG_INSTANT = "xxxxxx";
 
     @BeforeAll
     public static void startServer() {
@@ -27,16 +32,27 @@ class IdpCertSimpleClientTest {
         idpCertSimpleClient = new IdpCertSimpleClient(client);
     }
     @Test
-    void getSPIDCertData() throws CertDataTagListNotFoundException, CertDataNotFoundException {
-        List<IdpCertData> response = idpCertSimpleClient.getCertData(SPID_ENTITY_ID, instant);
+    void certSPIDDataFound() throws CertDataTagListNotFoundException, CertDataNotFoundException {
+        List<IdpCertData> response = idpCertSimpleClient.getCertData(SPID_ENTITY_ID, INSTANT);
 
         Assertions.assertNotNull(response);
     }
 
     @Test
-    void getCIECertData() throws CertDataTagListNotFoundException, CertDataNotFoundException {
-        List<IdpCertData> response = idpCertSimpleClient.getCertData(CIE_ENTITY_ID, instant);
+    void certCIEDataFound() throws CertDataTagListNotFoundException, CertDataNotFoundException {
+        List<IdpCertData> response = idpCertSimpleClient.getCertData(CIE_ENTITY_ID, INSTANT);
 
         Assertions.assertNotNull(response);
     }
+
+    @Test
+    void getCertDataWrongEntityID() throws CertDataTagListNotFoundException, CertDataNotFoundException {
+        Assertions.assertThrows(CertDataNotFoundException.class, () -> idpCertSimpleClient.getCertData(WRONG_ENTITY_ID, INSTANT));
+    }
+
+    @Test
+    void getCertDataWrongInstant() throws CertDataTagListNotFoundException, CertDataNotFoundException {
+        Assertions.assertThrows(CertDataTagListNotFoundException.class, () -> idpCertSimpleClient.getCertData(SPID_ENTITY_ID, WRONG_INSTANT));
+    }
+
 }
