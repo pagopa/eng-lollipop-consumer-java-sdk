c919cde7ac7f18653bd5c9e41b2fbbd96081d914
giomella <gioele.mella.st@everis.nttdata.com>
2023-04-13 14:47:25 +0200

    [SLS-21] added request headers validation.

---
+++ core/src/main/java/it/pagopa/tech/lollipop/consumer/model/RSAPublicKey.java
@@ -0,0 +1,15 @@
+package it.pagopa.tech.lollipop.consumer.model;
+
+import lombok.Data;
+
+/**
+ * RSA public key model
+ */
+@Data
+public class RSAPublicKey {
+
+    private String alg;
+    private String e;
+    private String kty;
+    private String n;
+}
