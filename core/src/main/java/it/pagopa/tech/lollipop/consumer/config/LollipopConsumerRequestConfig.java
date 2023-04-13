89d4d07adbff898d6403fd462d18d73eacee8f20
giomella <gioele.mella.st@everis.nttdata.com>
2023-04-13 11:05:02 +0200

    [SLS-21] added all request headers.

--- core/src/main/java/it/pagopa/tech/lollipop/consumer/config/LollipopConsumerRequestConfig.java
+++ core/src/main/java/it/pagopa/tech/lollipop/consumer/config/LollipopConsumerRequestConfig.java
@@ -17,6 +17,11 @@ public class LollipopConsumerRequestConfig {
 
     @Builder.Default private String originalMethodHeader = "x-pagopa-lollipop-original-method";
     @Builder.Default private String originalURLHeader = "x-pagopa-lollipop-original-url";
+    @Builder.Default private String assertionRefHeader = "x-pagopa-lollipop-assertion-ref";
+    @Builder.Default private String assertionTypeHeader = "x-pagopa-lollipop-assertion-type";
+    @Builder.Default private String userIdHeader = "x-pagopa-lollipop-user-id";
+    @Builder.Default private String publicKeyHeader = "x-pagopa-lollipop-public-key";
+    @Builder.Default private String authJWTHeader = "x-pagopa-lollipop-auth-jwt";
 
     @Builder.Default private String expectedFirstLcOriginalMethod = "POST";
     @Builder.Default private String expectedFirstLcOriginalUrl = " https://api-app.io.pagopa.it/first-lollipop/sign";
