a375fe17cb21a1689dbbd25e0c3a66573a4b9103
Alessio Cialini <acialini@external.nexigroup.com>
2023-04-18 19:17:40 +0200

    [SLS-12] Updated tests an docs

--- spring-impl/src/test/java/it/pagopa/tech/lollipop/consumer/spring/HttpVerifierHandlerInterceptorIntegrationTest.java
+++ spring-impl/src/test/java/it/pagopa/tech/lollipop/consumer/spring/HttpVerifierHandlerInterceptorIntegrationTest.java
@@ -6,6 +6,7 @@ import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 import it.pagopa.tech.lollipop.consumer.spring.config.HttpVerifierConfiguration;
 import it.pagopa.tech.lollipop.consumer.spring.config.SpringLollipopConsumerRequestConfig;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
@@ -41,7 +43,7 @@ public class HttpVerifierHandlerInterceptorIntegrationTest {
     @Autowired private HttpVerifierHandlerInterceptor interceptor;
 
     @Test
-    public void test() {
+    public void testWithValidRequestReturnsSuccess() {
         AssertionSimpleClientTestUtils.createExpectationAssertionFound();
 
         String signatureInput =
@@ -82,11 +84,63 @@ public class HttpVerifierHandlerInterceptorIntegrationTest {
                             request.getHeaders().add("Signature", signature);
                         });
 
-        String response =
-                exec.postForObject(
+        ResponseEntity<String> response =
+                exec.postForEntity(
                         "http://localhost:" + port,
                         "{\"message\":\"a valid message payload\"}",
                         String.class);
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(200, response.getStatusCodeValue());
+    }
+
+    public void testWithinvalidPayloadRequestReturnsUnauthorized() {
+        AssertionSimpleClientTestUtils.createExpectationAssertionFound();
+
+        String signatureInput =
+                "sig123=(\"content-digest\" \"x-pagopa-lollipop-original-method\""
+                        + " \"x-pagopa-lollipop-original-url\");created=1678293988;nonce=\"aNonce\";alg=\"ecdsa-p256-sha256\";keyid=\"sha256-a7qE0Y0DyqeOFFREIQSLKfu5WlbckdxVXKFasfcI-Dg\"";
+        var signature =
+                "sig123=:lTuoRytp53GuUMOB4Rz1z97Y96gfSeEOm/xVpO39d3HR6lLAy4KYiGq+1hZ7nmRFBt2bASWEpen7ov5O4wU3kQ==:";
+
+        RestTemplate exec = restTemplate.getRestTemplate();
+        exec.getClientHttpRequestInitializers()
+                .add(
+                        request -> {
+                            request.getHeaders()
+                                    .add(
+                                            "Content-Digest",
+                                            "sha-256=:cpyRqJ1VhoVC+MSs9fq4/4wXs4c46EyEFriskys43Zw=:");
+                            request.getHeaders()
+                                    .add(
+                                            "x-pagopa-lollipop-original-url",
+                                            "https://api-app.io.pagopa.it/first-lollipop/sign");
+                            request.getHeaders().add("x-pagopa-lollipop-original-method", "POST");
+                            request.getHeaders()
+                                    .add(
+                                            "x-pagopa-lollipop-public-key",
+                                            "eyJrdHkiOiJFQyIsIngiOiJGcUZEd"
+                                                    + "XdFZ3U0TVVYRVJQTVZMLTg1cEd2MkQzWW1MNEoxZ2ZNa2RiYzI0IiwieSI6Im"
+                                                    + "hkVjBveG1XRlN4TW9KVURwZGlocjc2clM4VlJCRXFNRmViWXlBZks5LWsiLCJjcnYiO"
+                                                    + "iJQLTI1NiJ9");
+                            request.getHeaders()
+                                    .add(
+                                            "x-pagopa-lollipop-assertion-ref",
+                                            "sha256-_ZzL8qeuAM5kQ9pbMB4tn7IDSQZCVXAkW9fm4P7ULPI");
+                            request.getHeaders().add("x-pagopa-lollipop-assertion-type", "SAML");
+                            request.getHeaders().add("x-pagopa-lollipop-auth-jwt", "aValidJWT");
+                            request.getHeaders()
+                                    .add("x-pagopa-lollipop-user-id", "AAAAAA89S20I111X");
+                            request.getHeaders().add("Signature-Input", signatureInput);
+                            request.getHeaders().add("Signature", signature);
+                        });
+
+        ResponseEntity<String> response =
+                exec.postForEntity(
+                        "http://localhost:" + port,
+                        "{\"message\":\"an invalid message payload\"}",
+                        String.class);
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(401, response.getStatusCodeValue());
     }
 
     @AfterAll
