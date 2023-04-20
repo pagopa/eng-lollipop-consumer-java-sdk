651872d432d2038c98bc66da8693ecbfde6e783f
Alessio Cialini <acialini@external.nexigroup.com>
2023-04-20 18:59:00 +0200

    [SLS-18] Introducing LollipopLogbackLoggerServiceFactoryTest

---
+++ core/src/test/java/it/pagopa/tech/lollipop/consumer/logger/impl/LollipopLogbackLoggerServiceFactoryTest.java
@@ -0,0 +1,17 @@
+/* (C)2023 */
+package it.pagopa.tech.lollipop.consumer.logger.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+class LollipopLogbackLoggerServiceFactoryTest {
+
+    @SneakyThrows
+    @Test
+    public void instanceIsCreated() {
+        assertThat(new LollipopLogbackLoggerServiceFactory().create())
+                .isInstanceOf(LollipopLogbackLoggerService.class);
+    }
+}
