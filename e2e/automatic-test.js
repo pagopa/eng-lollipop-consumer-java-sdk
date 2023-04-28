ef4cbfd19eebcbd23b73fadcb793c19292f10f0e
svariant <samuele.varianti@nttdata.com>
2023-04-28 12:38:15 +0200

    [SLS-16] Newman failed assertion test

--- e2e/automatic-test.js
+++ e2e/automatic-test.js
@@ -3,7 +3,8 @@ const newman = require('newman');
 newman.run({
    collection: require('./collections/LollipopSDKTest.postman_collection.json'),
    environment: './env/Lollipop environment variables.postman_environment.json',
-   reporters: 'htmlextra'
+   reporters: 'cli',
+   bail: true
 }, function (err, summary) {
   if (err) { throw err; }
   console.info('collection run complete!');
