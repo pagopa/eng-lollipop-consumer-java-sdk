fe2fe8215181e6db76b56c86a4f88d01139e74ea
svariant <samuele.varianti@nttdata.com>
2023-05-02 18:48:48 +0200

    [SLS-16] Newman verbose

--- e2e/automatic-test.js
+++ e2e/automatic-test.js
@@ -4,13 +4,15 @@ newman.run({
    collection: require('./collections/lollipopSDKTest.postman_collection.json'),
    environment: './env/lollipopEnvironmentVariables.postman_environment.json',
    reporters: ['cli', 'htmlextra'],
-   bail: true
+   bail: true,
+   verbose: true
 }, function (err, summary) {
   if (err) {  throw err; }
   if(summary?.run?.error){ throw 'collection run encountered an error.';}
 
   if(summary?.run?.failures?.length > 0){
     const errors = summary.run.failures;
+
     throw `following collection tests failed: ${errors.map((er) => `\n${er.source.name}`)}`;
   }
 
