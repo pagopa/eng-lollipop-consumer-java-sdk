97977327cd7b6cc94c1aa401cee5bd783e729433
mottone-dgs <119047198+mottone-dgs@users.noreply.github.com>
2025-12-18 11:51:24 +0100

    Develop (#114)
    * PN-17422 - rimossi i log relativi a familyName e name (#107)
    
    * PN-17422 - rimossi i log relativi a familyName e name
    
    * PN-17422 - rimossi i log relativi a familyName e name
    
    * Fix develop branch version
    
    * Revert "Fix develop branch version"
    
    This reverts commit d8cbb2b471452bdefe0906bc1b432e9be37e56f9.
    
    * Fix develop branch version (#111)
    
    * Bump package version to 1.2.0-RC.1
    
    * Revert "Bump package version to 1.2.0-RC.1"
    
    This reverts commit 20035005f5c2ebebe1da89c87fe3dd369895ddf6.
    
    * Bump package version to 1.2.0-RC.1 (#112)
    
    * PN-17420 - bump package version to 1.2.0 (#113)
    
    ---------
    
    Co-authored-by: paolavitidicaraffa <paola.vitidicaraffa@dgsspa.com>

--- core/src/main/java/it/pagopa/tech/lollipop/consumer/model/CommandResult.java
+++ core/src/main/java/it/pagopa/tech/lollipop/consumer/model/CommandResult.java
@@ -1,7 +1,6 @@
 /* (C)2023-2025 */
 package it.pagopa.tech.lollipop.consumer.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,13 +12,16 @@ public class CommandResult {
 
     private String resultCode;
     private String resultMessage;
+    @ToString.Exclude
     private String name; // user name
+    @ToString.Exclude
     private String familyName; // user familyName
 
     public CommandResult(String resultCode, String resultMessage) {
         this.resultCode = resultCode;
         this.resultMessage = resultMessage;
     }
+
     public CommandResult(String resultCode, String resultMessage, String name, String familyName) {
         this.resultCode = resultCode;
         this.resultMessage = resultMessage;
